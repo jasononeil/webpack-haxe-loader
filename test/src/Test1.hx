4cc6dcb313ca6b22b65aff92f1b8d71a37a58db4
Philippe Elsass <philippe.elsass@massive.co>
2018-04-23 18:32:20 +0100

    Haxe 4 changes `trace` output to include the hx source line

--- test/src/Test1.hx
+++ test/src/Test1.hx
@@ -1,6 +1,6 @@
 class Test1 {
     static function main() {
         trace('Test 1 OK');
-        trace(Webpack.require('./test.json'));
+        var json = Webpack.require('./test.json');
     }
 }
