abd198c2a271d28806762d8b54afe3641b392935
Rudy Ges <k@klabz.org>
2018-07-03 11:01:23 +0200

    Handle sub errors (base field is defined here, defined in this class) (#45)
    * Handle sub errors (base field is defined here, defined in this class)
    
    * Handle more sub errors + fix field overload sub error
    
    * Handle more errors, including overload resolution failure
    
    * Only display first and last source code lines of multiline sub errors
    
    * Added missing sub error
    
    * Fix typos

--- errorFormatter.js
+++ errorFormatter.js
@@ -16,12 +16,33 @@ function formatError(error) {
     const file = removeLoaders(error.file);
     const src = fs.readFileSync(file, {encoding: 'utf-8'});
 
-    return concat(
+    const ret = concat(
         `${baseError} in ${file}`,
         '',
         error.message,
         '',
         displaySource(src, error.positions),
+        ''
+    );
+
+    if (error.contextErrors) {
+        error.contextErrors.forEach(error => ret.push(formatSubError(error)));
+    }
+
+    return ret.join('\n');
+}
+
+function formatSubError(error) {
+    const baseError = formatTitle('error', '->');
+
+    const file = removeLoaders(error.file);
+    const src = fs.readFileSync(file, {encoding: 'utf-8'});
+    const indent = '  ';
+
+    return concat(
+        indent + baseError + ' ' + error.message,
+        indent + chalk.grey(file),
+        displaySource(src, error.positions, indent, 1, 0, 1),
         ''
     ).join('\n');
 }
@@ -37,7 +58,7 @@ function formatWarning(warning) {
         '',
         warning.message,
         '',
-        displaySource(src, warning.positions, 5, 0, 2),
+        displaySource(src, warning.positions, '', 5, 0, 2),
         ''
     ).join('\n');
 }
@@ -77,6 +98,7 @@ function removeLoaders(file) {
 function displaySource(
     src,
     [line, endLine, column, endColumn],
+    indent = '',
     maxLines = MAX_LINES,
     linesAround = LINES_AROUND,
     linesInside = LINES_INSIDE
@@ -109,14 +131,14 @@ function displaySource(
 
         if (isHighlighted && nbLines > maxLines) {
             if (i - line == linesInside) {
-                ret.push(chalk.grey(' [...]'));
+                ret.push(indent + chalk.grey(' [...]'));
                 continue;
             } else if (i - line > linesInside && endLine - i >= linesInside) {
                 continue;
             }
         }
 
-        let lineStr = color(isHighlighted ? '> ' : '  ');
+        let lineStr = indent + color(isHighlighted ? '> ' : '  ');
         lineStr += color(lpad(i, String(end).length) + ' | ');
 
         if (isHighlighted && line == endLine && column >= 0) {
@@ -186,19 +208,123 @@ function format(errors, type) {
 function groupErrors(errors) {
     const map = {};
     const other = [];
+    let previous = null;
 
     errors.forEach(error => {
         if (error.type != 'Haxe Error') return other.push(error);
 
-        const key = error.file + ':' + error.positions.join(':');
+        const key = flattenPosition(error);
+        let isSub = false;
 
-        if (map[key]) map[key].message += '\n' + error.message;
-        else map[key] = error;
+        if (isSubError(error, previous)) {
+            isSub = true;
+            if (!previous.contextErrors) {
+                previous.contextErrors = [error];
+            } else {
+                const previousSub = previous.contextErrors[previous.contextErrors.length - 1];
+                const prevKey = flattenPosition(previousSub);
+
+                if (key === prevKey) previousSub.message += '\n       ' + error.message
+                else previous.contextErrors.push(error);
+            }
+        } else if (!ignoreError(error)) {
+            if (map[key]) map[key].message += '\n' + error.message;
+            else map[key] = error;
+        }
+
+        if (!isSub) previous = error;
     });
 
     const groupedErrors = [];
     for (let k in map) if (map.hasOwnProperty(k)) groupedErrors.push(map[k]);
     return groupedErrors.concat(other);
+}
+
+function flattenPosition(error) {
+    return error.file + ':' + error.positions.join(':');
+}
+
+function ignoreError(error) {
+    if (error.message === 'End of overload failure reasons') return true;
+
+    return false;
+}
+
+function isSubError(error, previous) {
+    if (!previous || !previous.message) return false;
+    if (error.message === 'Defined in this class') return true;
+
+    if (previous.message === 'Could not find a suitable overload, reasons follow') {
+        return (error.message !== 'End of overload failure reasons');
+    }
+
+    if (previous.message === 'Extern constructor could not be inlined') {
+        if (error.message === 'Cancellation happened here') return true;
+        if (error.message === 'Variable is used here') return true;
+
+        return false;
+    }
+
+    if (
+        error.message === 'Add @:isVar here to enable it'
+        && previous.message === 'This field cannot be accessed because it is not a real variable'
+    ) return true;
+
+    if (
+        error.message === 'While specializing this call'
+        && previous.message === 'Only generic type parameters can be constructed'
+    ) return true;
+
+    if (error.message === 'Previously defined here') {
+        if (previous.message === 'Duplicate key') return true;
+        if (previous.message.startsWith('Duplicate access modifier')) return true;
+
+        return false;
+    }
+
+    if (
+        error.message === 'Conflicts with this'
+        && previous.message.startsWith('Conflicting access modifier')
+    ) return true;
+
+    if (
+        error.message.indexOf('Accessor method is here') > 0
+        && previous.message.indexOf('Macro methods cannot be used as property accessor') > 0
+    ) return true;
+
+    const fieldRedefine = /Cannot redefine field [^\s]+ with different type/;
+    if (fieldRedefine.test(previous.message)) {
+        if (error.message.startsWith('First type was')) return true;
+        if (error.message.startsWith('Second type was')) return true;
+
+        return false;
+    }
+
+    const fieldOverload = /Field [^\s]+ overloads parent class with different or incomplete type/;
+    if (
+        error.message === 'Base field is defined here'
+        && fieldOverload.test(previous.message)
+    ) return true;
+
+    const interfaceField = /Field [^\s]+ has different type than in [^\s]+/;
+    if (
+        error.message === 'Interface field is defined here'
+        && interfaceField.test(previous.message)
+    ) return true;
+
+    const fieldHasNoExpression = /Field [^\s]+ has no expression \(possible typing order issue\)/;
+    if (
+        error.message.startsWith('While building')
+        && fieldHasNoExpression.test(previous.message)
+    ) return true;
+
+    const fieldTypeMismatch = /Cannot create field [^\s]+ due to type mismatch/;
+    if (
+        error.message === 'Conflicting field was defined here'
+        && fieldTypeMismatch.test(previous.message)
+    ) return true;
+
+    return false;
 }
 
 module.exports = format;
